8da3ee2eff2b400a2a084b7551bf33814d4bb032
LNHuyVu <lenguyenhuyvu.htn@gmail.com>
2025-04-04 13:48:46 +0700

    First commit

---
+++ utils/data/projects-data.js
@@ -0,0 +1,57 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: "Quản lý dự án BIM",
+    description:
+      "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
+    tools: [
+      "Express",
+      "MongoDB",
+      "OpenAI API",
+      "MiniO",
+      "Node Mailer",
+      "PM2",
+      "Nginx",
+      "React",
+      "Tailwind",
+      "ShadcnUI",
+      "Redux",
+    ],
+    role: "Backend Developer 70%, Frontend Developer 30%",
+    code: "Private",
+    demo: "https://platform.syncbim.com",
+  },
+  {
+    id: 2,
+    name: "WebChat SPM",
+    description:
+      "I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.",
+    tools: [
+      "NextJS",
+      "Tailwind CSS",
+      "Google Maps",
+      "NestJS",
+      "TypeScript",
+      "MySQL",
+      "AWS S3",
+      "Sun-Editor",
+      "Gmail Passkey",
+    ],
+    role: "Full Stack Developer",
+    code: "",
+    demo: "",
+  },
+];
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+// },
